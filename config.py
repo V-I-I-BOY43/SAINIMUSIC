2f61abf32dd543c98ab08b6cb145b47762051be4
V-I-I-BOY43 <147950486+V-I-I-BOY43@users.noreply.github.com>
2024-11-04 09:58:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SURAJSAINI93/SAINIMUSIC",
+    "https://github.com/V-I-I-BOY43/SAINIMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
